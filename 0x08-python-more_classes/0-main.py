39c39af3a2d46ee9ed7030fc0bd5dbf891b9d271
tenderheartviv <vivian.emenike@yahoo.com>
2023-07-03 19:14:56 -0700

    0x08-python-more_classes

---
+++ 0x08-python-more_classes/0-main.py
@@ -0,0 +1,7 @@
+guillaume@ubuntu:~/0x08$ cat 0-main.py
+#!/usr/bin/python3
+Rectangle = __import__('0-rectangle').Rectangle
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
