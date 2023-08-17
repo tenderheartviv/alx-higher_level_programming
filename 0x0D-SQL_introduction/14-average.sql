a60a859bf755e68c4023f1e77d285c169542c97e
tenderheartviv <vivian.emenike@yahoo.com>
2023-08-16 19:13:56 -0700

    average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- Script to compute the average score of all records in second_table
-
-SELECT AVG(score) AS average FROM hbtn_0c_0.second_table; 
+-- Computes the average score of all records in the table second_table.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`; 
