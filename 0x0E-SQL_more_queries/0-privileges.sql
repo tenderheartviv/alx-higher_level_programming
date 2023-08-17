317287fb5393ff6f512159a9e797adee1603adaa
tenderheartviv <vivian.emenike@yahoo.com>
2023-08-16 18:53:52 -0700

    my first sql_queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,8 @@
---list all privileges of the MYSQL users user_0d_1 and user_0d_2
-mysql -u root -p -e "SHOW GRANTS FOR 'user_0d_1'@'localhost'; SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+-- Script to list privileges of MySQL users user_0d_1 and user_0d_2
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,11 @@
--- Create user if not exists
+-- Script to create MySQL user user_0d_1 with all privileges
+
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to the user on all databases and tables
+-- Grant all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
+
