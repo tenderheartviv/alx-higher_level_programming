533dae342a0d41a65ef925309b7f90c7930795a7
tenderheartviv <vivian.emenike@yahoo.com>
2023-09-14 16:46:30 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 
-exports.callMeMoby = function (x, theFunction) {
-  for (let a = 0; a < x; a++) {
-    theFunction();
-  }
+const myArgs = process.argv.slice(2);
+
+if (!myArgs[0] || !myArgs[2]) {
+  console.log(0);
+} else {
+  const args = myArgs.map(Number);
+  const firstMax = Math.max.apply(null, args);
+  args.splice(args.indexOf(firstMax), 1);
+  console.log(Math.max.apply(null, args));
+}
