f9d9f37f735dbc302f1eebbcfcd4233f8c361d09
tenderheartviv <vivian.emenike@yahoo.com>
2023-11-14 11:18:06 -0800

    javascript_object_scopes_closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,32 @@
 #!/usr/bin/node
-// concats 2 files
+
 const fs = require('fs');
-const x  = fs.readFileSync(process.argv[2], 'utf8');
-const y  = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], x + y);
+
+function concatFiles(file1Path, file2Path, destinationPath) {
+  try {
+    const content1 = fs.readFileSync(file1Path, 'utf-8');
+    const content2 = fs.readFileSync(file2Path, 'utf-8');
+
+    fs.writeFileSync(destinationPath, content1 + content2);
+
+    console.log(`Files ${file1Path} and ${file2Path} successfully concatenated to ${destinationPath}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+if (require.main === module) {
+  if (process.argv.length !== 5) {
+    console.log('Usage: ./script.js <file1Path> <file2Path> <destinationPath>');
+    process.exit(1);
+  }
+
+  const file1Path = process.argv[2];
+  const file2Path = process.argv[3];
+  const destinationPath = process.argv[4];
+
+  concatFiles(file1Path, file2Path, destinationPath);
+}
+
+module.exports = concatFiles;
+
