c38e949741a95fc6d7621bfa43da510019ee642f
tenderheartviv <vivian.emenike@yahoo.com>
2023-11-30 15:02:48 -0800

    0x10-python_network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Peak-finding algorithm."""
 
+
 def find_peak(list_of_integers):
     """Return the peak in a list of unsorted integers."""
     if list_of_integers == []:
@@ -14,19 +15,9 @@ def find_peak(list_of_integers):
 
     mid = int(n / 2)
     peak = list_of_integers[mid]
-    print(f"Mid: {mid}, Peak: {peak}")
-
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
     elif peak < list_of_integers[mid - 1]:
-        print("Going left")
         return find_peak(list_of_integers[:mid])
     else:
-        print("Going right")
         return find_peak(list_of_integers[mid + 1:])
-
-# Test your function
-test_list = [1, 3, 20, 4, 1, 0]
-result = find_peak(test_list)
-print(f"Peak: {result}")
-
